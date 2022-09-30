e501dc0672bb426e475fdce286dd1dcbcf350402
Mark Norton <us.marknorton@gmail.com>
2022-09-30 22:55:48 +0300

    basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 const App = () => {
+  const signed = true
+  return signed ? (
+    <Routes>
+      <Route path='' element={<Layout />}></Route>
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
+  ) : (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='register' element={<Register />} />
+      <Route path='*' element={<Navigate to='/register' />} />
+    </Routes>
+  )
+}
+
+const Layout = () => {
   return (
     <div className='App'>
-      <h1>chat</h1>
+      <Home />
+      <Outlet />
     </div>
   )
 }
